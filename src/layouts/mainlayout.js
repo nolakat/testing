efe950eda7807a285c72c821f7a0b28ad0acf381
Katie Nolan <nolakat@gmail.com>
2019-07-14 17:17:15 -0400

    resolve window undefined error

--- src/layouts/mainlayout.js
+++ src/layouts/mainlayout.js
@@ -5,61 +5,68 @@ import Header from '../components/Header';
 import { Helmet } from 'react-helmet'
 
 
-const MainLayout = ({ children }) =>{
+class MainLayout extends React.Component{
 
+    componentDidMount() {
+     console.log('mounted!');
+     this.location = window.location.href;
 
-    return(
-        <>
-            <Global styles={css`
+      }
 
-                body{
-                    margin: 0px;
-                    padding: 0px;
-                    min-height: 100vh;
-                    width: 100%;
-                    overflow-x: hidden;
-                    cursor: none;
-                }
+    render(){
+        return(
+            <>
+                <Global styles={css`
 
-                main{
-                    height: 100vh;
-                }
+                    body{
+                        margin: 0px;
+                        padding: 0px;
+                        min-height: 100vh;
+                        width: 100%;
+                        overflow-x: hidden;
+                        cursor: none;
+                    }
 
-                a{
-                    // cursor: none;
-                    border: 1px solid orange;
-                }
-
-                a:hover, a:active, a:focus{
-                    cursor: none;
-                }
-
-                header{
-
-                    padding: 1rem 0;
-                    display: flex;
-                    justify-content: space-between;
-                    border: 1px solid red;
+                    main{
+                        height: 100vh;
+                    }
 
                     a{
-                        margin: 0px 20px;
-                        border: 2px solid pink;
+                        // cursor: none;
+                        border: 1px solid orange;
                     }
-                }
-            `} />
-                <Helmet>
-                    <title> Hello There </title>
 
-                </Helmet>
-                <TestCursor location={window.location.href} />
-                <Header / >
-                <main id="main-container">
+                    a:hover, a:active, a:focus{
+                        cursor: none;
+                    }
 
-                {children}
+                    header{
 
-                </main>
-        </>
-    )
+                        padding: 1rem 0;
+                        display: flex;
+                        justify-content: space-between;
+                        border: 1px solid red;
+
+                        a{
+                            margin: 0px 20px;
+                            border: 2px solid pink;
+                        }
+                    }
+                `} />
+                    <Helmet>
+                        <title> Hello There </title>
+
+                    </Helmet>
+                    <TestCursor location={this.location} />
+                    <Header / >
+                    <main id="main-container">
+
+                    {this.props.children}
+
+                    </main>
+            </>
+        )
+    }
 
 }
 
